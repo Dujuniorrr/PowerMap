97cda9b551db26843ce0b8b4883772b40b9863a2
Dujuniorrr <durvaljunior117@gmail.com>
2024-03-07 17:52:55 -0300

    [feat] add directions in map

--- app/src/main/java/com/ifbaiano/powermap/fragment/MapFragment.java
+++ app/src/main/java/com/ifbaiano/powermap/fragment/MapFragment.java
@@ -6,11 +6,7 @@ import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
-import android.location.Address;
-import android.location.Geocoder;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,7 +16,6 @@ import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
-import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -33,25 +28,9 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.libraries.places.api.Places;
-import com.google.android.libraries.places.api.model.Place;
-import com.google.android.libraries.places.api.net.FindCurrentPlaceRequest;
-import com.google.android.libraries.places.api.net.FindCurrentPlaceResponse;
 import com.google.android.libraries.places.api.net.PlacesClient;
 import com.ifbaiano.powermap.R;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Locale;
-import java.util.Objects;
-
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
+import com.ifbaiano.powermap.service.PlaceService;
 
 public class MapFragment extends Fragment implements OnMapReadyCallback {
 
@@ -120,11 +99,8 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
 
                                 mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
 
-                                String url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" +
-                                        location.getLatitude() + "," + location.getLongitude() +
-                                        "&radius=10000&type=ev_charging_station&key=" + requireContext().getString(R.string.maps_key);
-                                Log.d("MAPS LOG DE DURVAl", url);
-                                new PlacesTask(mMap).execute(url);
+
+                                new PlaceService(latLng, requireContext(), mMap).execute("https://places.googleapis.com/v1/places:searchNearby");
                             }
                         } else {
                             Toast.makeText(requireContext(), "Não foi possível obter a localização atual", Toast.LENGTH_SHORT).show();
@@ -144,63 +120,6 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
                 onMapReady(mMap);
             } else {
                 Toast.makeText(requireContext(), "Permissão de localização negada", Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
-    // AsyncTask para realizar a solicitação HTTP
-    private static class PlacesTask extends AsyncTask<String, Void, String> {
-
-        private GoogleMap mMap;
-
-        public PlacesTask(GoogleMap mMap) {
-            this.mMap = mMap;
-        }
-
-        @Override
-        protected String doInBackground(String... urls) {
-            OkHttpClient client = new OkHttpClient();
-            Request request = new Request.Builder()
-                    .url(urls[0])
-                    .build();
-            try {
-                Response response = client.newCall(request).execute();
-                if (response.isSuccessful()) {
-                    return response.body().string();
-                } else {
-                    Log.e("PlacesTask", "Failed to fetch nearby EV charging stations");
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            super.onPostExecute(result);
-            if (result != null) {
-                try {
-                    JSONObject jsonObject = new JSONObject(result);
-
-                    JSONArray resultsArray = jsonObject.getJSONArray("results");
-
-                    for (int i = 0; i < resultsArray.length(); i++) {
-                        JSONObject placeObject = resultsArray.getJSONObject(i);
-                        String name = placeObject.getString("name");
-                        Log.d("NAME LOCATION", name);
-                        JSONObject geometry = placeObject.getJSONObject("geometry");
-                        JSONObject location = geometry.getJSONObject("location");
-                        double lat = location.getDouble("lat");
-                        double lng = location.getDouble("lng");
-
-                        mMap.addMarker(new MarkerOptions()
-                                .position(new LatLng(lat, lng))
-                                .title(name)
-                                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
-                    }
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
             }
         }
     }
